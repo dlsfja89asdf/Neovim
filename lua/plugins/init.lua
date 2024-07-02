3f6411611adef40eecf7df62f2227177b21a2251
123 <123@123.123>
2024-07-02 12:03:19 +0300

    Removed lazy=false from treesitter but does not actually change anything

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,7 +25,6 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-        lazy = false,
         config = function()
             require('nvim-treesitter.configs').setup({
                 ensure_installed = {
